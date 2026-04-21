fd617bcc8670129503e3fa34b495bb63f1117e10
djwARI <dave@artrobbins.com>
2026-04-20 18:17:04 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,4 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Gryphon_SW/',
   plugins: [react()],
-});
+})
