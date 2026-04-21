adce80b6188c3427b4adc9e50b69718b8d217bfa
djwARI <dave@artrobbins.com>
2026-04-20 19:02:26 -0700

    Condense vite.config.ts into a single line

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,1 @@
-import { defineConfig } from 'vite';
-
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: '/Gryphon_SW/',
-  plugins: [react()],
-})
+import { defineConfig } from 'vite'; import react from '@vitejs/plugin-react'; export default defineConfig({ base: '/Gryphon_SW/', plugins: [react()] });
